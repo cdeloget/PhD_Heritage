4daa59e631f8d0bc81da58d4350fa147683db6e5
CD <cypriend3@gmail.com>
2022-10-27 18:24:06 +0200

    commentaires

--- PhD_Heritage_MAIN.r
+++ PhD_Heritage_MAIN.r
@@ -3,6 +3,7 @@ library(igraph)
 library(rvest)
 
 discipline <- readline("Discipline : ") #taper "Géographie"
+
 motcle <- readline("mot clé ? : ")
 
 basepage <- paste("https://theses.fr/fr/?q=",motcle,"&checkedfacets=discipline=",discipline, sep="")
@@ -11,6 +12,6 @@ print(basepage)
 
 page_accueil <- read_html(basepage)
 
-result_zone <- page_accueil %>% html_nodes("div#resultat") %>% html_nodes("div.informations") %>% html_text() %>% as.data.frame(x=., row.names="TEST")
+result_zone <- page_accueil %>% html_nodes("div#resultat") %>% html_nodes("div.informations") %>% html_text() %>% as.data.frame(x=.)
 
 result_zone$.
