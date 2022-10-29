0221a6c1813528d33683fa8c3485fe6e74486e15
CD <cypriend3@gmail.com>
2022-10-30 01:31:03 +0200

    essai de geocodage avec API BAN en GET

--- PhD_Heritage_MAIN.r
+++ PhD_Heritage_MAIN.r
@@ -79,11 +79,11 @@ build_phd_table <- function(results, export=T){
   LIENS <- data.frame(ID_AUTEUR= id_auteur, AUTEUR=auteurs_theses, ANNEE= dates_theses, ID_DIR=id_dirtheses, DIR=directeur_theses, UNIV_DIR= univ_theses, INTITULE = noms_theses, DISCIPLINE = discipline_theses)
   
   
-  if (export==T){
+  if (export==T){#si utilisateur a choisi export en csv :
     write.csv(x = LIENS, file='LIENS.csv')
   }
   
-  return(LIENS)
+  return(LIENS)#on retourne un df de toute les soutenances
 
 }
 
@@ -92,28 +92,21 @@ build_phd_table <- function(results, export=T){
 #----------------------- ESSAIS ---------------------------------------
 #------------------------------------------------------------------
 
-# repcsv <- httr::text_content(
-#   POST(url="adresse.data.gouv.fr/search/csv/", 
-#        body= list(data=upload_file("LIENS.csv"), columns="UNIV_DIR")
-#   ))
-# 
-# repcsv
-# 
-# ### Géocodage de l'emplacement des universités
-# nom_univ_search <- paste("Université", LIENS$UNIV_DIR)
-# nom_univ_search <- str_replace_all(nom_univ_search," ", "%20")
-# nom_univ_search[1]
-# url_apiban <- paste("https://api-adresse.data.gouv.fr/search/?q=",nom_univ_search[1],"&type=street&limit=3", sep="")
-# 
-# adresses <- geojson_sf(
-#   httr::content(#on lit le contenu
-#     httr::GET(url_apiban),             # d'une requete GET
-#     as = "text",                # dont le résultat est stocké en texte dans R
-#     httr::content_type_json(),  # qui est fourni par l'API en json
-#     encoding = "UTF-8"          # encodage de la réponse de l'url
-#   )
-# )
 
+# geocode_from_a_column <- function(univ_tab, geoname_field){ #fonction à laquelle on passe le tableau, le champ du nom des université, et qui va effectuer un géocodage avec l'API BAN de la DINUM pour chacune d'entre elle
+nom_tab <- deparse(substitute(theses_liens))
+champ_a_traiter <- paste(nom_tab, geoname_field, sep="$")#on reconstitue en chaine de caractères la bonne colonne du tableau
+champ_a_traiter <- parse(text=champ_a_traiter)#on la transforme en expression
+noms_univ <- eval(champ_a_traiter) #executable avec eval(), dont on stocke le résultat dans une variable noms_univ
+noms_univ_complet <- paste("université", noms_univ)
+
+url_apiban <- paste("https://api-adresse.data.gouv.fr/search/?limit=1&q=",noms_univ[2], sep="")
+adresse <- fromJSON(httr::content(#on lit le contenu
+  httr::GET(url_apiban),
+  type="json",
+  as="text"
+))
+bibi <- as.data.frame(adresse$features$properties)
 
 
 #--------------------------------------------------------------------
@@ -125,8 +118,8 @@ discipline_saisie <- readline("Discipline : ") #ex : Taper "Geographie"
 
 motcles_saisis <- readline("mots clés ? : ")#ex : Taper "Thérèse Saint-Julien" ou "mobilités ferroviairew" 
 
-resultats <- get_resultats(discipline_saisie, motcles_saisis)
-theses_liens <- build_phd_table(resultats)
+resultats <- get_resultats(discipline_saisie, motcles_saisis)#on va requeter theses.fr et renvoyer le code html contenant les resultats de la recherche sur theses.fr
+theses_liens <- build_phd_table(resultats)#recup des informations importantes et mis en forme dans un tableau df
+# geocode_from_a_column(theses_liens, "UNIV_DIR")#ajout d'un geocodage d'une des colonne de notre choix (ici l'université de soutenance)
 
-get_academic_network
 
